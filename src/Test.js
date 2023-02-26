fc8234254c7871e9ef7e5a5ee2f041017b99fe7b
cnl-95 <98615179+cnl-95@users.noreply.github.com>
2023-02-26 13:46:13 -0500

    Created a responsive react app
    testing. js is reacting on local host (y)

---
+++ src/Test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Test() {
+    return (
+        <div>
+            
+           <center>Skunch smells</center> 
+           
+        </div>
+    )
+}
