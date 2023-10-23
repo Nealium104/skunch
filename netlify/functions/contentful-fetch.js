83fe36389b9f953ebece63208e02a47ec789f722
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-10-23 17:42:02 -0400

    tried a different fetch

---
+++ netlify/functions/contentful-fetch.js
@@ -0,0 +1,21 @@
+const contentful = require("contentful");
+
+const client = contentful.createClient({
+  space: process.env.CONTENTFUL_SPACE_API_KEY,
+  accessToken: process.env.CONTENTFUL_DELIVERY_API,
+});
+
+exports.handler = async () => {
+  try {
+    const entries = await client.getEntries();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(entries.items),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    };
+  }
+};
