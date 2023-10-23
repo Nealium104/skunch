7ef6334a11ef032c3785d577218d85795c5c4aee
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-10-23 13:36:22 -0400

    took off cloudinary integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,49 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import NetlifyCMS from "astro-netlify-cms";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: "git-gateway",
+          branch: "main",
+        },
+        // media_library: {
+        //   name: "cloudinary",
+        //   config: {
+        //     cloud_name: "dwbwyzsyp",
+        //     api_key: 718734367618239,
+        //     use_secure_url: true,
+        //   },
+        // },
+        site_url: "https://skunch.netlify.app/",
+        display_url: "https://skunch.netlify.app/",
+        logo_url: "/images/logoSmall.png",
+        collections: [
+          {
+            name: "posts",
+            label: "Posts",
+            folder: "src/pages/posts",
+            create: true,
+            fields: [
+              { label: "Title", name: "title", widget: "string" },
+              {
+                label: "Photos",
+                name: "photos",
+                widget: "list",
+                fields: [
+                  { label: "Image", name: "image", widget: "image" },
+                  { label: "Caption", name: "caption", widget: "text" },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    }),
+  ],
 });
---
+++ src/pages/posts/burl.md
@@ -0,0 +1,6 @@
+---
+title: Burl
+photos:
+  - image: /img_0037-medium-.jpg
+    caption: This is my test image
+---
