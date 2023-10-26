5d2231bf4e16582302a8a0096f37880ed571a311
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-10-26 09:16:25 -0400

    Contentful added on skunchbucket

--- src/lib/contentful.js
+++ src/lib/contentful.js
@@ -1,7 +1,9 @@
 import contentful from "contentful";
 
 export const contentfulClient = contentful.createClient({
-  space: process.env.CONTENTFUL_SPACE_API_KEY,
-  accessToken: process.env.CONTENTFUL_DELIVERY_API,
-  host: "cdn.contentful.com",
+  space: import.meta.env.CONTENTFUL_SPACE_ID,
+  accessToken: import.meta.env.DEV
+    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+  host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
 });
