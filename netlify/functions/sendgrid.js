912bac8d344eba3071397bcd77e8a976ec8b90cb
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-10-31 17:04:27 -0400

    changed email location

--- netlify/functions/sendgrid.js
+++ netlify/functions/sendgrid.js
@@ -2,19 +2,22 @@
 // https://github.com/sendgrid/sendgrid-nodejs
 
 const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: "neal.powers@outlook.com", // Change to your recipient
-  from: "neal.grindstaff@gmail.com", // Change to your verified sender
-  subject: "Sending with SendGrid is Fun",
-  text: "and easy to do anywhere, even with Node.js",
-  html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+
+exports.handler = async (event, context) => {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: "neal.powers@outlook.com", // Change to your recipient
+    from: "neal.grindstaff@gmail.com", // Change to your verified sender
+    subject: "Sending with SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log("Email sent");
-  })
-  .catch((error) => {
-    console.error(error);
-  });
