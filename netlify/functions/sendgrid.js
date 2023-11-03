a7f7338486d3f34a71e059925e6f66ecfcc9fb27
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-11-03 16:59:43 -0400

    made the endpoint in contact to the netlify function

--- netlify/functions/sendgrid.js
+++ netlify/functions/sendgrid.js
@@ -1,31 +1,28 @@
-// using Twilio SendGrid's v3 Node.js Library
-// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require("@sendgrid/mail");
 
 exports.handler = async (request, context) => {
-  const sgMail = require("@sendgrid/mail");
-  sgMail.setApiKey(Netlify.env.get("SENDGRID_API_KEY"));
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
   const msg = {
-    to: "neal.powers@outlook.com", // Change to your recipient
-    from: "neal.grindstaff@gmail.com", // Change to your verified sender
+    to: "neal.powers@outlook.com",
+    from: "neal.grindstaff@gmail.com",
     subject: "Sending with SendGrid is Fun",
     text: "and easy to do anywhere, even with Node.js",
     html: "<strong>and easy to do anywhere, even with Node.js</strong>",
   };
 
-  sgMail
-    .send(msg)
-    .then(() => {
-      console.log("Email sent");
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Email sent successfully" }),
-      };
-    })
-    .catch((error) => {
-      console.error(error);
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ message: "Error sending email" }),
-      };
-    });
+  try {
+    await sgMail.send(msg);
+    console.log("Email sent");
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Email sent successfully" }),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Error sending email" }),
+    };
+  }
 };
