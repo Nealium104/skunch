2da4101c1a0f1038c6fc3b686e4f2be7bfcab4b0
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-11-03 17:20:38 -0400

    changed email endpoint

--- netlify/functions/sendgrid.js
+++ netlify/functions/sendgrid.js
@@ -4,7 +4,7 @@ exports.handler = async (request, context) => {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
   const msg = {
-    to: "neal.powers@outlook.com",
+    to: "nealpowers@neal-powers.com",
     from: "neal.grindstaff@gmail.com",
     subject: "Sending with SendGrid is Fun",
     text: "and easy to do anywhere, even with Node.js",
