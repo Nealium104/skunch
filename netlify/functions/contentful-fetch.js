0ff9919a7a649065ea4e45a5e81b1595d07e1228
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-11-09 22:18:31 -0500

    redid the commit

--- netlify/functions/contentful-fetch.js
+++ netlify/functions/contentful-fetch.js
@@ -1,8 +1,12 @@
 import { client } from "./contentful";
 
-exports.handler = async () => {
+exports.handler = async (event) => {
+  const { queryStringParameters } = event;
+  const contentType = queryStringParameters["content_type"];
   try {
-    const entries = await client.getEntries();
+    const entries = await client.getEntries({
+      content_type: contentType,
+    });
     return {
       statusCode: 200,
       body: JSON.stringify(entries.items),
