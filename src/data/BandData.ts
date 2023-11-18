68985218da2a086b9a567acb6796d64c980e9b07
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-11-17 19:32:29 -0500

    inverted icon

--- src/data/BandData.ts
+++ src/data/BandData.ts
@@ -1,37 +1,57 @@
-interface BandBios {
+interface Albums {
   name: string;
-  short: string;
-  long: string;
+  year: string;
+  tracklist: string[];
   img: string;
   width: number;
   height: number;
 }
 
-const BandBios: BandBios[] = [
+const Albums: Albums[] = [
   {
-    name: "Daric",
-    short: "I'm the frontman",
-    long: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    name: "The Fall of Skunch",
+    year: "2023",
+    tracklist: [
+      "Comfortable Silence",
+      "Castle",
+      "Left Out Of Love",
+      "Nick's House",
+    ],
     img: "https://res.cloudinary.com/dwbwyzsyp/image/upload/v1697907594/daric_vwb3os.jpg",
     width: 1358,
     height: 1698,
   },
   {
-    name: "Alex",
-    short: "Bass go burrrrrr",
-    long: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    name: "The Summer of Skunch",
+    year: "2023",
+    tracklist: [
+      "Can't Skate",
+      "Turning Yr Back",
+      "See the Sun",
+      "Nick's House",
+      "Can't Skate (Reprise)",
+      "Five Summers, Pt. 1",
+    ],
     img: "https://res.cloudinary.com/dwbwyzsyp/image/upload/v1697907594/alex_nwcdpd.jpg",
     width: 1440,
     height: 1800,
   },
   {
-    name: "Evan",
-    short: "Big drum go bonk",
-    long: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-    img: "https://res.cloudinary.com/dwbwyzsyp/image/upload/v1697907594/evan_enaope.jpg",
-    width: 1080,
-    height: 1079,
+    name: "Skunch",
+    year: "2019",
+    tracklist: [
+      "Not Worth Paying",
+      "Peninsula Blues",
+      "Old East Erie",
+      "Enough",
+      "Comfortable Silence",
+      "Meet You There",
+      "Desire Lines",
+    ],
+    img: "string",
+    width: 2018,
+    height: 203929,
   },
 ];
 
-export default BandBios;
+export default Albums;
